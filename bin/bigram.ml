3e3421a55008f441f5094b7145d50d2577517cfc
Derek Chiang <me@derekchiang.com>
2025-07-19 11:21:18 -0700

    Set up the forward pass

--- bin/bigram.ml
+++ bin/bigram.ml
@@ -7,6 +7,7 @@ module TDSL = Operation.TDSL
 module NTDSL = Operation.NTDSL
 module CDSL = Train.CDSL
 module Rand = Ir.Rand.Lib
+module Asgns = Ir.Assignments
 
 module type Backend = Ir.Backend_intf.Backend
 
@@ -38,61 +39,80 @@ let print_tensor t = Tensor.print ~here:[%here] ~with_code:false ~with_grad:fals
 let one_hot ~num_classes indices =
   let num_classes = num_classes - 1 in
   let%op classes = TDSL.range num_classes in
-  (* Broadcast indices to add a dimension for classes *)
-  let%op indices_expanded = indices ++ "i => i j" in
-  (* Broadcast classes to match indices dimension *)
-  let%op classes_expanded = classes ++ "j => i j" in
-  (* Compare expanded tensors *)
+  print_tensor classes;
+  let%op indices_expanded = indices ++ "b|1 => b|i" in
+  let%op classes_expanded = classes ++ "i => b|i" in
   let%op one_hot = indices_expanded = classes_expanded in
   one_hot
 
 let _print_range_tensor () =
+  let seed = 3 in
+  Rand.init seed;
+  Utils.settings.fixed_state_for_init <- Some seed;
+
   let module Backend = (val Backends.fresh_backend ()) in
   let stream = Backend.(new_stream @@ get_device ~ordinal:0) in
   let ctx = Backend.make_context stream in
   let upto = 5 in
-  let num_classes = 3 in
-  let%op tensor1 = TDSL.range upto in
-  let%op classes = TDSL.range num_classes in
+  (* let num_classes = 3 in *)
+  let%op tensor = TDSL.range upto in
+  (* let%op classes = TDSL.range num_classes in *)
 
-  (* Test different broadcasting approaches *)
-  let%op indices_expanded = tensor1 ++ "i => i j" in
-  let%op classes_expanded = classes ++ "j => i j" in
-  let%op comparison = indices_expanded = classes_expanded in
+  Train.forward_and_forget (module Backend) ctx tensor;
+  print_tensor tensor
 
-  printf "Testing one-hot encoding with broadcasting:\n";
-  printf "Indices: 0,1,2,3,4  Classes: 0,1,2\n";
-  printf "Expected: each row should be one-hot for that index\n";
-  Train.forward_and_forget (module Backend) ctx comparison;
-  print_tensor comparison
+(* let () = _print_range_tensor () *)
 
-(* let () = print_range_tensor () *)
+let tensor_of_int_list lst =
+  let size = List.length lst in
+  lst |> List.map ~f:Float.of_int |> Array.of_list
+  |> Tensor.ndarray ~batch_dims:[ size ] ~output_dims:[ 1 ]
 
 let () =
-  Rand.init 0;
+  let seed = 11 in
+  Rand.init seed;
+  Utils.settings.fixed_state_for_init <- Some seed;
 
   let module Backend = (val Backends.fresh_backend ()) in
   let stream = Backend.(new_stream @@ get_device ~ordinal:0) in
   let ctx = Backend.make_context stream in
   let bigrams = get_all_bigrams () |> bigrams_to_indices in
 
-  let test_bigrams = List.take bigrams 10 in
-  let int_input, _ = List.unzip test_bigrams in
-  let input_list = int_input |> List.map ~f:Float.of_int in
-  let input_arr = Array.of_list input_list in
-  let size = Array.length input_arr in
+  let batch_size = 5 in
+  let int_input, int_output = List.unzip (List.take bigrams batch_size) in
 
-  printf "First 10 character indices: %s\n"
-    (Array.to_list input_arr |> List.map ~f:Float.to_string |> String.concat ~sep:", ");
+  let input_tensor = tensor_of_int_list int_input in
+  let output_tensor = tensor_of_int_list int_output in
 
-  let tensor = TDSL.ndarray ~output_dims:[ size ] input_arr in
-  let one_hot_tensor = one_hot ~num_classes:27 tensor in
+  let inputs = input_tensor |> one_hot ~num_classes:27 in
+  let outputs = output_tensor |> one_hot ~num_classes:27 in
+  Train.set_hosted inputs.value;
 
-  (* Mark the tensor to be on host for printing *)
-  (* Train.set_on_host one_hot_tensor.value; *)
+  let random_weights = Array.init 27 ~f:(fun _ -> Random.float 2.0 -. 1.0) in
+  let w = TDSL.param ~values:random_weights ~output_dims:[ 27 ] "w" in
+  let%op logits = w *. inputs in
+  Train.set_hosted logits.value;
 
-  (* Compute the tensor *)
-  let routine = Train.to_routine (module Backend) ctx IDX.empty @@ Train.forward one_hot_tensor in
-  Train.run routine;
-  printf "One-hot encoding (first 10 chars, all 27 classes):\n";
-  print_tensor one_hot_tensor
+  let%op counts = exp logits in
+  Train.set_hosted counts.value;
+
+  let%op probs = counts /. (counts ++ "b|...->... => b|0") in
+  Train.set_hosted probs.value;
+
+  let%op output_probs = (probs *. outputs) ++ "b|...->... => b|0" in
+  Train.set_hosted output_probs.value;
+
+  let%op loss = neg (log output_probs) in
+  Train.set_hosted loss.value;
+
+  let%op total_loss = loss ++ "...|...->... => 0" in
+  Train.set_hosted total_loss.value;
+
+  Train.forward_and_forget (module Backend) ctx total_loss;
+  print_tensor inputs;
+  print_tensor logits;
+  print_tensor counts;
+  print_tensor probs;
+  print_tensor output_probs;
+  print_tensor loss;
+  print_tensor total_loss
